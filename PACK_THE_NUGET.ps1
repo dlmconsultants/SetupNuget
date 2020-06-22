4ab1c36c22c384804161d3cedd8a9dd98956599a
Alex Yates <alex.yates@dlmconsultants.com>
2020-06-22 10:53:33 +0100

    Adding example -OutputDirectory switch

--- PACK_THE_NUGET.ps1
+++ PACK_THE_NUGET.ps1
@@ -1,2 +1,5 @@
 # Pack the NuGet
-nuget pack "$PSScriptRoot\example_files\example.nuspec" -version "1.0"
+nuget pack "$PSScriptRoot\example_files\example.nuspec" -version "1.0" # -OutputDirectory "C:\out"
+
+# Docs:
+# https://docs.microsoft.com/en-us/nuget/reference/cli-reference/cli-ref-pack
